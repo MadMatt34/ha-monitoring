9bfe40181f7e8e6f241843207dacff8f78f1a759
MadMatt34 <64684388+MadMatt34@users.noreply.github.com>
2026-08-01 12:32:02 +0200

    Update __init__.py

--- custom_components/ha_monitoring/__init__.py
+++ custom_components/ha_monitoring/__init__.py
@@ -1,8 +1,9 @@
 """Intégration HA Monitoring pour la surveillance système."""
 import logging
+from .const import DOMAIN
 
-DOMAIN = "ha_monitoring"
 _LOGGER = logging.getLogger(__name__)
+
 
 async def async_setup(hass, config):
     """Configuration initiale de l'intégration HA Monitoring."""
