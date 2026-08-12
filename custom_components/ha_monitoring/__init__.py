9dc3e3b0270efb588b8151bb97938d1010ca58ed
MadMatt34 <64684388+MadMatt34@users.noreply.github.com>
2026-08-12 15:59:41 +0200

    Ruff correction

--- custom_components/ha_monitoring/__init__.py
+++ custom_components/ha_monitoring/__init__.py
@@ -4,7 +4,6 @@ from __future__ import annotations
 
 from typing import Any
 
-from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_validation as cv
