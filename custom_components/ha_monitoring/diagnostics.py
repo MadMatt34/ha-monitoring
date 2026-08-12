38aa64fb4ca548567800f9020af7820873edc8da
MadMatt34 <64684388+MadMatt34@users.noreply.github.com>
2026-08-12 15:38:10 +0000

    style: auto-format avec Ruff

--- custom_components/ha_monitoring/diagnostics.py
+++ custom_components/ha_monitoring/diagnostics.py
@@ -59,9 +59,7 @@ async def async_get_config_entry_diagnostics(
             "is_ready": coordinator._is_ready,
             "last_update_success": coordinator.last_update_success,
             "last_exception": (
-                str(coordinator.last_exception)
-                if coordinator.last_exception
-                else None
+                str(coordinator.last_exception) if coordinator.last_exception else None
             ),
             "data": (
                 async_redact_data(
