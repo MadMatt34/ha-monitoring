7aa7fa95df0a0cf68c007366cf21cb4814a6841e
MadMatt34 <64684388+MadMatt34@users.noreply.github.com>
2026-09-25 09:04:50 +0200

    initial options page simplified

--- custom_components/ha_monitoring/config_flow.py
+++ custom_components/ha_monitoring/config_flow.py
@@ -73,54 +73,8 @@ def _flatten_options(
     return flat
 
 
-def get_schema(
-    hass: HomeAssistant | None = None,
-    options: dict[str, Any] | None = None,
-) -> vol.Schema:
+def get_schema() -> vol.Schema:
     """Construit le schéma du formulaire initial."""
-    options = options or {}
-    current_interval = options.get(
-        CONF_SCAN_INTERVAL,
-        DEFAULT_SCAN_INTERVAL,
-    )
-    current_traces_interval = options.get(
-        CONF_TRACES_SCAN_INTERVAL,
-        DEFAULT_TRACES_SCAN_INTERVAL,
-    )
-    current_system_info_interval = options.get(
-        CONF_SYSTEM_INFO_SCAN_INTERVAL,
-        DEFAULT_SYSTEM_INFO_SCAN_INTERVAL,
-    )
-    current_timeout = options.get(
-        CONF_OFFLINE_TIMEOUT,
-        DEFAULT_OFFLINE_TIMEOUT,
-    )
-    current_startup_delay = options.get(
-        CONF_STARTUP_DELAY,
-        DEFAULT_STARTUP_DELAY,
-    )
-    current_excluded_domains = options.get(
-        CONF_EXCLUDED_UNAVAILABLE_DOMAINS,
-        DEFAULT_EXCLUDED_UNAVAILABLE_DOMAINS,
-    )
-    current_battery_threshold = options.get(
-        CONF_BATTERY_LOW_THRESHOLD,
-        DEFAULT_BATTERY_LOW_THRESHOLD,
-    )
-    current_excluded_batteries = options.get(
-        CONF_EXCLUDED_BATTERIES,
-        [],
-    )
-    domain_options: list[str] = []
-    allowed_domains: list[str] | None = None
-
-    if hass is not None:
-        entity_ids = hass.states.async_entity_ids()
-        all_domains = {entity_id.split(".", 1)[0] for entity_id in entity_ids}
-
-        domain_options = sorted(all_domains)
-        allowed_domains = sorted(all_domains - set(current_excluded_domains))
-
     return vol.Schema(
         {
             vol.Required("section_timings"): section(
@@ -128,7 +82,7 @@ def get_schema(
                     {
                         vol.Required(
                             CONF_STARTUP_DELAY,
-                            default=current_startup_delay,
+                            default=DEFAULT_STARTUP_DELAY,
                         ): selector.NumberSelector(
                             selector.NumberSelectorConfig(
                                 min=0,
@@ -140,7 +94,7 @@ def get_schema(
                         ),
                         vol.Required(
                             CONF_SCAN_INTERVAL,
-                            default=current_interval,
+                            default=DEFAULT_SCAN_INTERVAL,
                         ): selector.NumberSelector(
                             selector.NumberSelectorConfig(
                                 min=5,
@@ -152,7 +106,7 @@ def get_schema(
                         ),
                         vol.Required(
                             CONF_SYSTEM_INFO_SCAN_INTERVAL,
-                            default=current_system_info_interval,
+                            default=DEFAULT_SYSTEM_INFO_SCAN_INTERVAL,
                         ): selector.NumberSelector(
                             selector.NumberSelectorConfig(
                                 min=1,
@@ -164,7 +118,7 @@ def get_schema(
                         ),
                         vol.Required(
                             CONF_TRACES_SCAN_INTERVAL,
-                            default=current_traces_interval,
+                            default=DEFAULT_TRACES_SCAN_INTERVAL,
                         ): selector.NumberSelector(
                             selector.NumberSelectorConfig(
                                 min=1,
@@ -176,7 +130,7 @@ def get_schema(
                         ),
                         vol.Required(
                             CONF_OFFLINE_TIMEOUT,
-                            default=current_timeout,
+                            default=DEFAULT_OFFLINE_TIMEOUT,
                         ): selector.NumberSelector(
                             selector.NumberSelectorConfig(
                                 min=1,
@@ -188,7 +142,7 @@ def get_schema(
                         ),
                         vol.Required(
                             CONF_BATTERY_LOW_THRESHOLD,
-                            default=current_battery_threshold,
+                            default=DEFAULT_BATTERY_LOW_THRESHOLD,
                         ): selector.NumberSelector(
                             selector.NumberSelectorConfig(
                                 min=1,
@@ -196,155 +150,6 @@ def get_schema(
                                 step=1,
                                 mode="slider",
                                 unit_of_measurement="%",
-                            )
-                        ),
-                    }
-                ),
-                {"collapsed": True},
-            ),
-            vol.Required("section_exclusions_system"): section(
-                vol.Schema(
-                    {
-                        vol.Optional(
-                            CONF_EXCLUDED_ADDONS,
-                            default=options.get(CONF_EXCLUDED_ADDONS) or [],
-                        ): selector.SelectSelector(
-                            selector.SelectSelectorConfig(
-                                options=[],
-                                custom_value=True,
-                                multiple=True,
-                            )
-                        ),
-                        vol.Optional(
-                            CONF_EXCLUDED_INTEGRATIONS,
-                            default=options.get(CONF_EXCLUDED_INTEGRATIONS) or [],
-                        ): selector.SelectSelector(
-                            selector.SelectSelectorConfig(
-                                options=[],
-                                custom_value=True,
-                                multiple=True,
-                            )
-                        ),
-                        vol.Optional(
-                            CONF_EXCLUDED_REPAIRS,
-                            default=options.get(CONF_EXCLUDED_REPAIRS) or [],
-                        ): selector.SelectSelector(
-                            selector.SelectSelectorConfig(
-                                options=[],
-                                custom_value=True,
-                                multiple=True,
-                            )
-                        ),
-                    }
-                ),
-                {"collapsed": True},
-            ),
-            vol.Required("section_exclusions_updates"): section(
-                vol.Schema(
-                    {
-                        vol.Optional(
-                            CONF_EXCLUDED_UPDATES,
-                            default=options.get(CONF_EXCLUDED_UPDATES) or [],
-                        ): selector.EntitySelector(
-                            selector.EntitySelectorConfig(
-                                domain="update",
-                                multiple=True,
-                            )
-                        ),
-                    }
-                ),
-                {"collapsed": True},
-            ),
-            vol.Required("section_exclusions_scripts"): section(
-                vol.Schema(
-                    {
-                        vol.Optional(
-                            CONF_EXCLUDED_AUTOMATIONS,
-                            default=options.get(CONF_EXCLUDED_AUTOMATIONS) or [],
-                        ): selector.EntitySelector(
-                            selector.EntitySelectorConfig(
-                                domain="automation",
-                                multiple=True,
-                            )
-                        ),
-                        vol.Optional(
-                            CONF_EXCLUDED_SCRIPTS,
-                            default=options.get(CONF_EXCLUDED_SCRIPTS) or [],
-                        ): selector.EntitySelector(
-                            selector.EntitySelectorConfig(
-                                domain="script",
-                                multiple=True,
-                            )
-                        ),
-                    }
-                ),
-                {"collapsed": True},
-            ),
-            vol.Required("section_exclusions_offline"): section(
-                vol.Schema(
-                    {
-                        vol.Optional(
-                            CONF_EXCLUDED_OFFLINE,
-                            default=options.get(CONF_EXCLUDED_OFFLINE) or [],
-                        ): selector.DeviceSelector(selector.DeviceSelectorConfig(multiple=True)),
-                    }
-                ),
-                {"collapsed": True},
-            ),
-            vol.Required("section_exclusions_unavailable"): section(
-                vol.Schema(
-                    {
-                        vol.Optional(
-                            CONF_EXCLUDED_UNAVAILABLE_DOMAINS,
-                            default=current_excluded_domains,
-                        ): selector.SelectSelector(
-                            selector.SelectSelectorConfig(
-                                options=domain_options,
-                                custom_value=False,
-                                multiple=True,
-                                mode=selector.SelectSelectorMode.DROPDOWN,
-                            )
-                        ),
-                        vol.Optional(
-                            CONF_EXCLUDED_UNAVAILABLE_GLOBS,
-                            default=options.get(CONF_EXCLUDED_UNAVAILABLE_GLOBS) or [],
-                        ): selector.SelectSelector(
-                            selector.SelectSelectorConfig(
-                                options=[],
-                                custom_value=True,
-                                multiple=True,
-                            )
-                        ),
-                        vol.Optional(
-                            CONF_EXCLUDED_UNAVAILABLE_ENTITIES,
-                            default=options.get(CONF_EXCLUDED_UNAVAILABLE_ENTITIES) or [],
-                        ): selector.EntitySelector(
-                            selector.EntitySelectorConfig(
-                                multiple=True,
-                                filter=(
-                                    selector.EntityFilterSelectorConfig(domain=allowed_domains)
-                                    if allowed_domains
-                                    else None
-                                ),
-                            )
-                        ),
-                    }
-                ),
-                {"collapsed": True},
-            ),
-            vol.Required("section_exclusions_battery"): section(
-                vol.Schema(
-                    {
-                        vol.Optional(
-                            CONF_EXCLUDED_BATTERIES,
-                            default=current_excluded_batteries,
-                        ): selector.EntitySelector(
-                            selector.EntitySelectorConfig(
-                                multiple=True,
-                                filter=selector.EntityFilterSelectorConfig(
-                                    domain="sensor",
-                                    device_class="battery",
-                                ),
                             )
                         ),
                     }
@@ -577,7 +382,9 @@ def get_exclusions_schema(
                         vol.Optional(
                             CONF_EXCLUDED_OFFLINE,
                             default=options.get(CONF_EXCLUDED_OFFLINE) or [],
-                        ): selector.DeviceSelector(selector.DeviceSelectorConfig(multiple=True)),
+                        ): selector.DeviceSelector(
+                            selector.DeviceSelectorConfig(multiple=True)
+                        ),
                     }
                 ),
                 {"collapsed": True},
@@ -598,7 +405,8 @@ def get_exclusions_schema(
                         ),
                         vol.Optional(
                             CONF_EXCLUDED_UNAVAILABLE_GLOBS,
-                            default=options.get(CONF_EXCLUDED_UNAVAILABLE_GLOBS) or [],
+                            default=options.get(CONF_EXCLUDED_UNAVAILABLE_GLOBS)
+                            or [],
                         ): selector.SelectSelector(
                             selector.SelectSelectorConfig(
                                 options=[],
@@ -608,12 +416,17 @@ def get_exclusions_schema(
                         ),
                         vol.Optional(
                             CONF_EXCLUDED_UNAVAILABLE_ENTITIES,
-                            default=options.get(CONF_EXCLUDED_UNAVAILABLE_ENTITIES) or [],
+                            default=options.get(
+                                CONF_EXCLUDED_UNAVAILABLE_ENTITIES
+                            )
+                            or [],
                         ): selector.EntitySelector(
                             selector.EntitySelectorConfig(
                                 multiple=True,
                                 filter=(
-                                    selector.EntityFilterSelectorConfig(domain=allowed_domains)
+                                    selector.EntityFilterSelectorConfig(
+                                        domain=allowed_domains
+                                    )
                                     if allowed_domains
                                     else None
                                 ),
@@ -823,7 +636,7 @@ class HAMonitoringConfigFlow(
 
         return self.async_show_form(
             step_id="user",
-            data_schema=get_schema(self.hass),
+            data_schema=get_schema(),
         )
 
     @staticmethod
